1f1a348b47794b512f9713fac1038e7ec7054c67
rajesh kumar <rajeshvk2003@gmail.com>
2024-05-06 01:05:56 +0530

    first commit

---
+++ Backened/index.js
@@ -0,0 +1,7 @@
+import expess from 'express'
+
+const app=expess();
+
+app.listen('3000',()=>{
+     console.log('app is running on port 3000!!!');
+})
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
-
+import {BrowserRouter ,Route,Routes} from 'react-router-dom'
+import Home from './Pages/Home'
+import SignIn from './Pages/SignIn'
+import SignUp from './Pages/SignUp'
+import About from './Pages/About'
+import Header from './Components/Header'
 export default function App() {
-  return (
-    <div>App</div>
-  )
+  return <BrowserRouter>
+  <Header/>
+   <Routes>
+   <Route path='/' element={<Home />} />
+   <Route path='/sign-in' element={<SignIn />} />
+   <Route path='/sign-up' element={<SignUp />} />
+    <Route path='/about' element={<About/>}/>
+   
+   
+   
+   </Routes>
+  
+    
+    
+   
+  </BrowserRouter>
+    
+  
 }
